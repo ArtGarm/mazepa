84d1c1046cab2f7df560becf6dc256d38689fc05
Radion <radion91@gmail.com>
2015-10-05 13:35:01 +0200

    index10%

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
+	<div class="main">
+		<section>
+			<div class="mbox">
+				<div class="index_biography_wrap">
+					<div class="index_biography cfix">
+						<div class="index_biography_left">
+							<div class="index_biography_img">
+								<img src="images/index_bio_img.png" alt="">
+							</div>
+						</div>
+						<div class="index_biography_right">
+							<div class="index_biography_text">
+								<div class="index_biography_title">
+									Игорь Александрович Мазепа
+								</div>
+								<div class="index_biography_desc">
+									<p>Генеральный директор Concorde Capital, член наблюдательного совета Государственного  экспортно-импортного банка Украины, председатель биржевого совета ОАО «Украинская биржа», председатель правления Бизнес-совета «Цена государства»
+									</p>
+									<p>Родился 2 июля 1976 в Киеве — генеральный директор одной из крупнейшей в Украине инвестиционной компании Concorde Capital (Украина, Киев).</p>
+									<p>Получил высшее экономическое и юридическое образование в Киевском национальном экономическом университете по специальностям «Международная экономика» и «Право».</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
 	</div>
