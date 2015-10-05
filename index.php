69ea43e9ab9b3c228aec3952d85c557fe214e5e7
Radion <radion91@gmail.com>
2015-10-05 14:11:00 +0200

    index 40%

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 	<div class="main">
 		<section>
 			<div class="mbox">
-				<div class="index_biography_wrap">
+				<div class="index_biography_wrap index_sect_1">
 					<div class="index_biography cfix">
 						<div class="index_biography_left">
 							<div class="index_biography_img">
@@ -26,9 +26,24 @@
 			</div>
 		</section>
 		<section>
-			<div class="index_section_wrap">
+			<div class="index_section_wrap index_sect_2">
 				<div class="index_section">
-
+					<div class="section_title">
+						<div class="section_title_line">
+							<span>Карьера</span>
+						</div>
+					</div>
+					<div class="section_bg_img">
+						<img src="images/index_sect_1_bg.jpg" alt="">
+					</div>
+					<div class="mbox">
+						<div class="index_section_text">
+							<p>В 1997 году начал карьеру инвестиционного банкира в российско-американской инвестиционной компании (ИК) Prospect Investments. В 2000—2002 годах занимал пост управляющего директора в ИК  Foyil Securities New Europe, затем — директора в ИК МФК.</p>
+							<p>В 2004 году основал и возглавил ИК Concorde Capital, является ее генеральным директором. На протяжении многих лет компания входит в тройку лучших инвестиционных банков Украины (по данным исследований крупнейшего международного информационного агентства Thomson Reuters).</p>
+							<p>В 2008 году был избран председателем биржевого совета ОАО «Украинская биржа» и занимает данный пост по сегодняшний день. </p>
+							<p>В 2014 году назначен членом Наблюдательного совета крупнейшего государственного банка «Укрэксимбанк».  Также c июля по сентябрь 2014 года был членом Наблюдательного совета публичного акционерного общества «Альфа-Банк». </p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
