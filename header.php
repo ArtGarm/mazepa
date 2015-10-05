2354d9b383bd211415a39d4d5c49e00f0ea1c107
Dmitry <dmitry1940@mail.ru>
2015-10-05 17:29:09 +0300

    header finished

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<title></title>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:300italic,300,400,400italic,500,500italic,700,700italic,900italic,900&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
 
