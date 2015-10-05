21edd9e1e7127e9ccd9bf6b862eee9d7e2d86fda
ArtGarm <artem.garmatyk@mail.ru>
2015-10-05 14:41:44 +0300

    sdfg

---
+++ partials/foto.php
@@ -0,0 +1,52 @@
+<div class="foto-Wrap">
+    <div class="foto-box">
+        <div class="foto"> <img src="images/foto1.png" alt="" /> </div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"> <img src="images/foto2.png" alt="" /> </div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"> <img src="images/foto3.png" alt="" /> </div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"> <img src="images/foto4.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto5.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto6.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto7.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto8.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto9.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+    <div class="foto-box">
+        <div class="foto"><img src="images/foto10.png" alt="" /></div>
+        <div class="ivent"></div>
+        <div class="date-ivent"></div>
+    </div>
+</div>
