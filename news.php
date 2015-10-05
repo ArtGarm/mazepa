15b071217641440c6c23c4029bff1b45b514af78
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-05 14:40:44 +0300

    no message

--- news.php
+++ news.php
@@ -1,1 +1,44 @@
-<p class="qwe">qwertyuioppasdfgjhjklzxcvbbnm</p>
+<div class="news-main-wrap">
+	<div class="mbox">
+		<div class="news-main">
+			<div class="hot-news-wrap">
+				<div class="hot-news">
+					<div class="title">
+						<p>Главные новости</p>
+					</div>
+					<div class="hot-new">
+						<div class="image-wrap">
+							<div class="image">
+								<img src="images/hotnew.jpg" alt="" />
+							</div>
+						</div>
+						<div class="hot-new-body-wrap">
+							<div class="hot-new-body">
+								<div class="date-wrap">
+									<div class="day">
+										<span>01</span>
+									</div>
+									<div class="mount-time">
+										<div class="mount">
+											<p>Сентября 2015</p>
+										</div>
+										<div class="time">
+											<span>15 : 47</span>
+										</div>
+									</div>
+								</div>
+								<div class="new-title">
+									<h2>ТО ВЗЛЕТ, ТО ПОСАДКА</h2>
+								</div>
+								<div class="new-contant">
+									<p><span>Игорь Мазепа был самым успешным инвестбанкиром Украины. Что с ним сейчас?</span><br/>Таким образом новая модель организационной деятельности требуют определения и уточнения существенных финансовых и административных условий. Товарищи! дальнейшее развитие различных форм деятельности способствует подготовки и реализации новых предложений. Повседневная практика показывает, что новая модель... организационной деятельности в значительной степени обуславливает создание... системы обучения кадров, соответствует насущным потребностям. Задача организации, в особенности же реализация намеченных плановых заданий редставляет собой интересный эксперимент проверки дальнейших направлений развития.</p>
+								</div>
+								<a href="">Читать дальше</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
