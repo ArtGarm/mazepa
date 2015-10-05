5deb64d8fcfabdb664a96b6ccbdb72972f7feea1
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-05 16:47:33 +0300

    news almost ready

--- news.php
+++ news.php
@@ -1,40 +1,226 @@
 <div class="news-main-wrap">
-	<div class="mbox">
-		<div class="news-main">
-			<div class="hot-news-wrap">
-				<div class="hot-news">
-					<div class="title">
-						<p><span>Главные новости</span></p>
+	<div class="news-main">
+		<div class="hot-news-wrap">
+			<div class="hot-news">
+				<div class="section_title_red">
+					<div class="section_title_line_red">
+						<span>Главные новости</span>
 					</div>
-					<div class="hot-new">
-						<div class="image-wrap">
-							<div class="image">
-								<img src="images/hotnew.jpg" alt="" />
+				</div>
+				<div class="mbox">
+					<div class="hot-slider">
+						<div class="hot-new">
+							<div class="image-wrap">
+								<div class="image">
+									<img src="images/hotnew.jpg" alt="" />
+								</div>
+							</div>
+							<div class="hot-new-body-wrap">
+								<div class="hot-new-body">
+									<div class="date-wrap">
+										<div class="day">
+											<span>01</span>
+										</div>
+										<div class="mount-time">
+											<div class="mount">
+												<p>Сентября 2015</p>
+											</div>
+											<div class="time">
+												<span>15 : 47</span>
+											</div>
+										</div>
+									</div>
+									<div class="new-title">
+										<h2>ТО ВЗЛЕТ, ТО ПОСАДКА</h2>
+									</div>
+									<div class="new-contant">
+										<p><span>Игорь Мазепа был самым успешным инвестбанкиром Украины. Что с ним сейчас?</span><br /><br />Таким образом новая модель организационной деятельности требуют определения и уточнения существенных финансовых и административных условий. Товарищи! дальнейшее развитие</p>
+									</div>
+									<a href="#">Читать дальше <span>&raquo;</span></a>
+								</div>
 							</div>
 						</div>
-						<div class="hot-new-body-wrap">
-							<div class="hot-new-body">
-								<div class="date-wrap">
-									<div class="day">
-										<span>01</span>
-									</div>
-									<div class="mount-time">
-										<div class="mount">
-											<p>Сентября 2015</p>
-										</div>
-										<div class="time">
-											<span>15 : 47</span>
-										</div>
-									</div>
+						<div class="hot-new">
+							<div class="image-wrap">
+								<div class="image">
+									<img src="images/hotnew.jpg" alt="" />
 								</div>
-								<div class="new-title">
-									<h2>ТО ВЗЛЕТ, ТО ПОСАДКА</h2>
+							</div>
+							<div class="hot-new-body-wrap">
+								<div class="hot-new-body">
+									<div class="date-wrap">
+										<div class="day">
+											<span>01</span>
+										</div>
+										<div class="mount-time">
+											<div class="mount">
+												<p>Сентября 2015</p>
+											</div>
+											<div class="time">
+												<span>15 : 47</span>
+											</div>
+										</div>
+									</div>
+									<div class="new-title">
+										<h2>ТО ВЗЛЕТ, ТО ПОСАДКА</h2>
+									</div>
+									<div class="new-contant">
+										<p><span>Игорь Мазепа был самым успешным инвестбанкиром Украины. Что с ним сейчас?</span><br /><br />Таким образом новая модель организационной деятельности требуют определения и уточнения существенных финансовых и административных условий. Товарищи! дальнейшее развитие</p>
+									</div>
+									<a href="#">Читать дальше <span>&raquo;</span></a>
 								</div>
-								<div class="new-contant">
-									<p><span>Игорь Мазепа был самым успешным инвестбанкиром Украины. Что с ним сейчас?</span><br /><br />Таким образом новая модель организационной деятельности требуют определения и уточнения существенных финансовых и административных условий. Товарищи! дальнейшее развитие</p>
+							</div>
+						</div>
+						<div class="hot-new">
+							<div class="image-wrap">
+								<div class="image">
+									<img src="images/hotnew.jpg" alt="" />
+								</div>
+							</div>
+							<div class="hot-new-body-wrap">
+								<div class="hot-new-body">
+									<div class="date-wrap">
+										<div class="day">
+											<span>01</span>
+										</div>
+										<div class="mount-time">
+											<div class="mount">
+												<p>Сентября 2015</p>
+											</div>
+											<div class="time">
+												<span>15 : 47</span>
+											</div>
+										</div>
+									</div>
+									<div class="new-title">
+										<h2>ТО ВЗЛЕТ, ТО ПОСАДКА</h2>
+									</div>
+									<div class="new-contant">
+										<p><span>Игорь Мазепа был самым успешным инвестбанкиром Украины. Что с ним сейчас?</span><br /><br />Таким образом новая модель организационной деятельности требуют определения и уточнения существенных финансовых и административных условий. Товарищи! дальнейшее развитие</p>
+									</div>
+									<a href="#">Читать дальше <span>&raquo;</span></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="news-wrap">
+			<div class="news">
+				<div class="section_title">
+					<div class="section_title_line">
+						<span>Все новости</span>
+					</div>
+				</div>
+				<div class="mbox">
+					<div class="blocks-news">
+						<div class="block-new">
+							<div class="new-img-wrap">
+								<img src="images/new3.jpg" alt=""/>
+							</div>
+							<div class="date-wrap">
+								<div class="day">
+									<span>21</span>
+								</div>
+								<div class="mount-time">
+									<div class="mount">
+										<p>Сентября 2015</p>
+									</div>
+									<div class="time">
+										<span>15 : 47</span>
+									</div>
 								</div>
 								<a href="#">Читать дальше <span>&raquo;</span></a>
 							</div>
+							<div class="block-new-title">
+								<p>Итоги конференции The Ukrainian Recipe: Reforms, Privatisation, Recovery</p>
+							</div>
+							<div class="block-new-contant">
+								<p>21 сентября представители украинского правительства, крупнейших отечественных и зарубежных компаний, хедж-фондов и мировых финансовых институтов обсудили экономическую ситуацию, ход реформ, приватизацию и инвестиционный климат...</p>
+							</div>
+						</div>
+						<div class="block-new">
+							<div class="new-img-wrap">
+								<img src="images/new1.jpg" alt=""/>
+							</div>
+							<div class="date-wrap">
+								<div class="day">
+									<span>21</span>
+								</div>
+								<div class="mount-time">
+									<div class="mount">
+										<p>Сентября 2015</p>
+									</div>
+									<div class="time">
+										<span>15 : 47</span>
+									</div>
+								</div>
+								<a href="#">Читать дальше <span>&raquo;</span></a>
+							</div>
+							<div class="block-new-title">
+								<p>Итоги конференции The Ukrainian Recipe: Reforms, Privatisation, Recovery</p>
+							</div>
+							<div class="block-new-contant">
+								<p>21 сентября представители украинского правительства, крупнейших отечественных и зарубежных компаний, хедж-фондов и мировых финансовых институтов обсудили экономическую ситуацию, ход реформ, приватизацию и инвестиционный климат...</p>
+							</div>
+						</div>
+						<div class="block-new">
+							<div class="new-img-wrap">
+								<img src="images/new3.jpg" alt=""/>
+							</div>
+							<div class="date-wrap">
+								<div class="day">
+									<span>21</span>
+								</div>
+								<div class="mount-time">
+									<div class="mount">
+										<p>Сентября 2015</p>
+									</div>
+									<div class="time">
+										<span>15 : 47</span>
+									</div>
+								</div>
+								<a href="#">Читать дальше <span>&raquo;</span></a>
+							</div>
+							<div class="block-new-title">
+								<p>Итоги конференции The Ukrainian Recipe: Reforms, Privatisation, Recovery</p>
+							</div>
+							<div class="block-new-contant">
+								<p>21 сентября представители украинского правительства, крупнейших отечественных и зарубежных компаний, хедж-фондов и мировых финансовых институтов обсудили экономическую ситуацию, ход реформ, приватизацию и инвестиционный климат...</p>
+							</div>
+						</div>
+						<div class="block-new">
+							<div class="new-img-wrap">
+								<img src="images/new1.jpg" alt=""/>
+							</div>
+							<div class="date-wrap">
+								<div class="day">
+									<span>21</span>
+								</div>
+								<div class="mount-time">
+									<div class="mount">
+										<p>Сентября 2015</p>
+									</div>
+									<div class="time">
+										<span>15 : 47</span>
+									</div>
+								</div>
+								<a href="#">Читать дальше <span>&raquo;</span></a>
+							</div>
+							<div class="block-new-title">
+								<p>Итоги конференции The Ukrainian Recipe: Reforms, Privatisation, Recovery</p>
+							</div>
+							<div class="block-new-contant">
+								<p>21 сентября представители украинского правительства, крупнейших отечественных и зарубежных компаний, хедж-фондов и мировых финансовых институтов обсудили экономическую ситуацию, ход реформ, приватизацию и инвестиционный климат...</p>
+							</div>
+						</div>
+					</div>
+					<div class="paginator-wrap">
+						<div class="paginator">
+							<a href="#" class="current-page">1</a>
+							<a href="#">2</a>
+							<a href="#">3</a>
 						</div>
 					</div>
 				</div>
