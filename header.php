7cb279e851cbd188d9cc789611a6dc08918de02b
Radion <radion91@gmail.com>
2015-10-05 12:04:45 +0200

    node modules

---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<title></title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300italic,300,400,400italic,500,500italic,700,700italic,900italic,900&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
+
+	<link rel="stylesheet" href="css/bootstrap.css" media="all" />
+	<link rel="stylesheet" href="css/bootstrap-theme.css" media="all" />
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+
+	<link rel="stylesheet" href="css/tuesdayAnimate.css" media='all' />
+
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+
+	<!-- bootstrap -->
+
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+
+	<!-- /bootstrap -->
+
+	<script src="js/selectivizr-min.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/jquery.znice.css" media="all" />
+	<script src="js/jquery.validate.min.js" type="text/javascript"></script>
+	<!--<script src="js/jquery.znice.validate.js" type="text/javascript"></script>
+	<script src="js/jquery.znice.js" type="text/javascript"></script>-->
+
+	<link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox.css" media="all" />
+	<script src="fancybox/jquery.fancybox.js" type="text/javascript"></script>
+
+	<script src="js/modernizr.js" type="text/javascript"></script>
+	<script src="js/jquery.watermark.min.js" type="text/javascript"></script>
+
+<!-- sliders -->
+	<link rel="stylesheet" type="text/css" href="css/slick.css" media="all" />
+	<script src="js/slick.js" type="text/javascript"></script>
+<!-- sliders -->
+
+<!-- jScrollPane -->
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+<!-- jScrollPane -->
+
+
+<!-- remove Files -->
+
+	<!--<script src="js/myalert.js" type="text/javascript"></script>
+	<script src="develop/autocssrenew.js" type="text/javascript"></script>-->
+
+    <link rel="stylesheet" type="text/css" href="css/develop_2.css" media="all" />
+    <script src="js/develop_2.js" type="text/javascript"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/develop_3.css" media="all" />
+    <script src="js/develop_3.js" type="text/javascript"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/develop_4.css" media="all" />
+    <script src="js/develop_4.js" type="text/javascript"></script>
+<!-- remove Files -->
+
+	<link rel="stylesheet" href="css/style_main.css" media="all" />
+	<script src="js/scr.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+		<script>
+		document.createElement('header');
+		document.createElement('nav');
+		document.createElement('section');
+		document.createElement('article');
+		document.createElement('aside');
+		document.createElement('footer');
+		document.createElement('time');
+		</script>
+		<script src="js/pie.js" type="text/javascript"></script>
+	<![endif]-->
+
+	<!--[if lt IE 8]><script src="js/oldie/warning.js"></script><script>window.onload=function(){e("js/oldie/")}</script><![endif]-->
+</head>
+<body>
+	<?partial('zHiddenBlock');?>
+	<header class="header">
+		<div class="mbox">
+
+		</div>
+	</header>
