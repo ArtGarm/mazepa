cddab9d756d072063b4ffa101baddb4bd03c3c34
ArtGarm <artem.garmatyk@mail.ru>
2015-10-05 17:46:22 +0300

    foto - video

--- partials/foto.php
+++ partials/foto.php
@@ -1,52 +1,122 @@
 <div class="foto-Wrap">
     <div class="foto-box">
-        <div class="foto"> <img src="images/foto1.png" alt="" /> </div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto11.jpg" class="fancy-box-foto">
+                <img src="images/foto1.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent">Конференция «Бизнес-2014»</div>
+        <div class="date-ivent"> 08.12.2014 </div>
     </div>
     <div class="foto-box">
-        <div class="foto"> <img src="images/foto2.png" alt="" /> </div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto12.jpg" class="fancy-box-foto">
+                <img src="images/foto2.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> Подписание контракта </div>
+        <div class="date-ivent"> 08.12.2014 </div>
     </div>
     <div class="foto-box">
-        <div class="foto"> <img src="images/foto3.png" alt="" /> </div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto13.jpg" class="fancy-box-foto">
+                <img src="images/foto3.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> По пути на конференцию </div>
+        <div class="date-ivent"> 08.12.2014 </div>
     </div>
     <div class="foto-box">
-        <div class="foto"> <img src="images/foto4.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto14.jpg" class="fancy-box-foto">
+                <img src="images/foto4.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent">Конференция «Бизнес-2014»</div>
+        <div class="date-ivent">08.12.2014</div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto5.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto15.jpg" class="fancy-box-foto">
+                <img src="images/foto5.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> Подписание контракта </div>
+        <div class="date-ivent"> 08.12.2014 </div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto6.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+         <div class="foto">
+            <a href="images/foto16.jpg" class="fancy-box-foto">
+                <img src="images/foto6.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent">По пути на конференцию </div>
+        <div class="date-ivent">08.12.2014</div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto7.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto17.jpg" class="fancy-box-foto">
+                <img src="images/foto7.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> Конференция «Бизнес-2014» </div>
+        <div class="date-ivent">08.12.2014</div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto8.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto18.jpg " class="fancy-box-foto">
+                <img src="images/foto8.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> Подписание контракта </div>
+        <div class="date-ivent">08.12.2014</div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto9.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto19.jpg" class="fancy-box-foto">
+                <img src="images/foto9.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent"> По пути на конференцию </div>
+        <div class="date-ivent">08.12.2014</div>
     </div>
     <div class="foto-box">
-        <div class="foto"><img src="images/foto10.png" alt="" /></div>
-        <div class="ivent"></div>
-        <div class="date-ivent"></div>
+        <div class="foto">
+            <a href="images/foto20.jpg" class="fancy-box-foto">
+                <img src="images/foto10.png" alt=""  class="main-foto"/ >
+                <img src="images/lupa.png" class="lupa" alt="" />
+            </a>
+        </div>
+        <div class="ivent" > Конференция «Бизнес-2014» </div>
+        <div class="date-ivent">08.12.2014</div>
+    </div>
+</div>
+
+<div class="block-Radick"></div>
+
+<div class="video-Wrap">
+    <div class="video-box">
+        <div class="video"></div>
+        <div class="ivent"> Конференция «Бизнес-2014» </div>
+        <div class="date-ivent"> 08.12.2014 </div>
+    </div>
+    <div class="video-box">
+        <div class="video"></div>
+        <div class="ivent"> Подписание контракта</div>
+        <div class="date-ivent">08.12.2014 </div>
+    </div>
+    <div class="video-box">
+        <div class="video"></div>
+        <div class="ivent"> По пути на конференцию  </div>
+        <div class="date-ivent"> 08.12.2014 </div>
     </div>
 </div>
