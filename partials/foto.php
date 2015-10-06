dc5e160022065d14691e2eafefe8d6a815f1297a
Radion <radion91@gmail.com>
2015-10-06 11:00:08 +0200

    fix foto

--- partials/foto.php
+++ partials/foto.php
@@ -1,143 +1,190 @@
-<div class="foto-Wrap">
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto11.jpg" class="fancy-box-foto">
-                <img src="images/foto1.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent">Конференция «Бизнес-2014»</div>
-        <div class="date-ivent"> 08.12.2014 </div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto12.jpg" class="fancy-box-foto">
-                <img src="images/foto2.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> Подписание контракта </div>
-        <div class="date-ivent"> 08.12.2014 </div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto13.jpg" class="fancy-box-foto">
-                <img src="images/foto3.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> По пути на конференцию </div>
-        <div class="date-ivent"> 08.12.2014 </div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto14.jpg" class="fancy-box-foto">
-                <img src="images/foto4.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent">Конференция «Бизнес-2014»</div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto15.jpg" class="fancy-box-foto">
-                <img src="images/foto5.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> Подписание контракта </div>
-        <div class="date-ivent"> 08.12.2014 </div>
-    </div>
-    <div class="foto-box">
-         <div class="foto">
-            <a href="images/foto16.jpg" class="fancy-box-foto">
-                <img src="images/foto6.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent">По пути на конференцию </div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto17.jpg" class="fancy-box-foto">
-                <img src="images/foto7.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> Конференция «Бизнес-2014» </div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto18.jpg " class="fancy-box-foto">
-                <img src="images/foto8.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> Подписание контракта </div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto19.jpg" class="fancy-box-foto">
-                <img src="images/foto9.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent"> По пути на конференцию </div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-    <div class="foto-box">
-        <div class="foto">
-            <a href="images/foto20.jpg" class="fancy-box-foto">
-                <img src="images/foto10.png" alt=""  class="main-foto"/ >
-                <img src="images/lupa.png" class="lupa" alt="" />
-            </a>
-        </div>
-        <div class="ivent" > Конференция «Бизнес-2014» </div>
-        <div class="date-ivent">08.12.2014</div>
-    </div>
-</div>
 
-<div class="block-Radick"></div>
+<div class="foto_main_wrap">
+    <div class="photo_section ">
+        <div class="news_line_wrap animate-section">
+            <div class="section_title animate-it">
+                <div class="section_title_line animate-it">
+                    <span>Фото</span>
+                </div>
+            </div>
+        </div>
+        <div class="mbox">
 
-<div class="video-Wrap">
-    <div class="video-box">
-        <div class="video">
-            <a href="#" class="fancy-box-video" target="_blank">
-                <img src="images/video1.png" alt=""  class="main-video"/ >
-                <div class="video-circle"></div>
-                <div class="video-circle-anim"></div>
-                <div class="video-triangle"></div>
-            </a>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto11.jpg" class="fancy-box-foto">
+                            <img src="images/foto1.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent">Конференция «Бизнес-2014»</div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto12.jpg" class="fancy-box-foto">
+                            <img src="images/foto2.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> Подписание контракта </div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto13.jpg" class="fancy-box-foto">
+                            <img src="images/foto3.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> По пути на конференцию </div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto14.jpg" class="fancy-box-foto">
+                            <img src="images/foto4.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent">Конференция «Бизнес-2014»</div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto15.jpg" class="fancy-box-foto">
+                            <img src="images/foto5.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> Подписание контракта </div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto16.jpg" class="fancy-box-foto">
+                            <img src="images/foto6.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent">По пути на конференцию </div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto17.jpg" class="fancy-box-foto">
+                            <img src="images/foto7.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> Конференция «Бизнес-2014» </div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto18.jpg " class="fancy-box-foto">
+                            <img src="images/foto8.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> Подписание контракта </div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto19.jpg" class="fancy-box-foto">
+                            <img src="images/foto9.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent"> По пути на конференцию </div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
+            <div class="foto-box animate-section">
+                <div class="animate-it foto-box-wrap">
+                    <div class="foto">
+                        <a href="images/foto20.jpg" class="fancy-box-foto">
+                            <img src="images/foto10.png" alt=""  class="main-foto"/ >
+                            <img src="images/lupa.png" class="lupa" alt="" />
+                        </a>
+                    </div>
+                    <div class="ivent" > Конференция «Бизнес-2014» </div>
+                    <div class="date-ivent">08.12.2014</div>
+                </div>
+            </div>
         </div>
-        <div class="ivent"> Конференция «Бизнес-2014» </div>
-        <div class="date-ivent"> 08.12.2014 </div>
     </div>
-    <div class="video-box">
-        <div class="video">
-            <a href="#" class="fancy-box-video" target="_blank">
-                <img src="images/video2.png" alt=""  class="main-video"/ >
-                <div class="video-circle"></div>
-                <div class="video-circle-anim"></div>
-                <div class="video-triangle"></div>
-            </a>
+
+
+    <div class="video_section">
+        <div class="news_line_wrap animate-section">
+            <div class="section_title animate-it">
+                <div class="section_title_line animate-it">
+                    <span>Видео</span>
+                </div>
+            </div>
         </div>
-        <div class="ivent"> Подписание контракта</div>
-        <div class="date-ivent">08.12.2014 </div>
-    </div>
-    <div class="video-box">
-        <div class="video">
-            <a href="#" class="fancy-box-video" target="_blank">
-                <img src="images/video3.png" alt=""  class="main-video"/ >
-                <div class="video-circle"></div>
-                <div class="video-circle-anim"></div>
-                <div class="video-triangle"></div>
-            </a>
+        <div class="video-Wrap">
+            <div class="video-box animate-section">
+                <div class="video-box-wrap animate-it">
+                    <div class="video">
+                        <a href="#" class="fancy-box-video" target="_blank">
+                            <img src="images/video1.png" alt=""  class="main-video"/ >
+                            <div class="video-circle"></div>
+                            <div class="video-circle-anim"></div>
+                            <div class="video-triangle"></div>
+                        </a>
+                    </div>
+                    <div class="ivent"> Конференция «Бизнес-2014» </div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
+            <div class="video-box animate-section">
+                <div class="video-box-wrap animate-it">
+                    <div class="video">
+                        <a href="#" class="fancy-box-video" target="_blank">
+                            <img src="images/video2.png" alt=""  class="main-video"/ >
+                            <div class="video-circle"></div>
+                            <div class="video-circle-anim"></div>
+                            <div class="video-triangle"></div>
+                        </a>
+                    </div>
+                    <div class="ivent"> Подписание контракта</div>
+                    <div class="date-ivent">08.12.2014 </div>
+                </div>
+            </div>
+            <div class="video-box animate-section">
+                <div class="video-box-wrap animate-it">
+                    <div class="video">
+                        <a href="#" class="fancy-box-video" target="_blank">
+                            <img src="images/video3.png" alt=""  class="main-video"/ >
+                            <div class="video-circle"></div>
+                            <div class="video-circle-anim"></div>
+                            <div class="video-triangle"></div>
+                        </a>
+                    </div>
+                    <div class="ivent"> По пути на конференцию  </div>
+                    <div class="date-ivent"> 08.12.2014 </div>
+                </div>
+            </div>
         </div>
-        <div class="ivent"> По пути на конференцию  </div>
-        <div class="date-ivent"> 08.12.2014 </div>
     </div>
 </div>
